c49c0fa43bfe878124fcf3eedf05cb93902e7dee
Renato Cassino <renatocassino@gmail.com>
2019-12-04 00:47:26 -0300

    Add terminal-game-io

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
-export const run = async () => {
-  console.log(111)
+import {
+  createTerminalGameIo,
+  ITerminalGameIoOptions,
+  ITerminalGameIo,
+  Key,
+} from 'terminal-game-io'
+
+let v = 1
+
+const frameHandler = (instance: ITerminalGameIo) => {
+  v += 1
+  if (v > 9) v = 1
+  instance.drawFrame('->' + v, 3, 1)
 }
+
+const keypressHandler = (instance: ITerminalGameIo, keyName: string) => {
+  //switch (keyName) {
+  //  case Key.Escape:
+      instance.exit()
+  //    break
+  //}
+}
+
+createTerminalGameIo({
+  fps: 12,
+  frameHandler,
+  keypressHandler,
+})
